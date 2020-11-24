caa5e566ec51236e5671754c377efe7abea9eeae
Markevich Anatoly <markduho0@gmail>
2020-11-24 23:21:55 +0300

    some changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,30 @@
-$(function() {
-  $('#license').change(function(){
-      $('.license-selected__box').hide();
-      $('#' + $(this).val()).show();
-  });
+
+
+// input 
+$('input[name="radio-license"]').click(function(){
+
+	var target = $('#block-' + $(this).val());
+
+ 
+
+  $('.license-selected__box').not(target).hide(0);
+
+
+	target.fadeIn(500);
+
 });
+
+$('input[name="radio-license"]').click(function(){
+
+	var target = $('#license-' + $(this).val());
+
+ 
+
+  $('.license-selected__box-1').not(target).hide(0);
+
+
+	target.fadeIn(500);
+
+});
+
+
